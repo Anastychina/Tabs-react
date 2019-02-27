83abdbf5426451ee7ea50f2e609afc8b473fe509
Anastasiia <anastychina@gmail.com>
2019-02-27 11:35:02 +0200

    using prettier

--- src/App.js
+++ src/App.js
@@ -2,37 +2,38 @@ import React, { useState } from 'react';
 import './App.css';
 
 const menu = [
-  {
-    'label': 'Tab1',
-    'content': 'One'
-  },
-  {
-    'label': 'Tab2',
-    'content': 'Two'
-  },
-  {
-    'label': 'Tab3',
-    'content': 'Three'
-  },
+	{
+		label: 'Tab1',
+		content: 'One',
+	},
+	{
+		label: 'Tab2',
+		content: 'Two',
+	},
+	{
+		label: 'Tab3',
+		content: 'Three',
+	},
 ];
 
 const Tab = () => {
-  const [active, changeActive] = useState(menu[0]);
+	const [active, changeActive] = useState(menu[0]);
 
-  const handleClick = (index) =>  changeActive(menu[index]);
-  return (
-    <div>
-      <div className="header">
-        {menu.map((item, index) =>
-          <div onClick = {() => handleClick(index)}
-               className = {item.label === active.label ? "active" : ""}>
-            {item.label}
-          </div>
-        )
-        }
-      </div>
-      <div className="footer">{active.content}</div>
-    </div>
-  )
+	const handleClick = index => changeActive(menu[index]);
+	return (
+		<div>
+			<div className="header">
+				{menu.map((item, index) => (
+					<div
+						onClick={() => handleClick(index)}
+						className={item.label === active.label ? 'active' : ''}
+					>
+						{item.label}
+					</div>
+				))}
+			</div>
+			<div className="footer">{active.content}</div>
+		</div>
+	);
 };
 export default Tab;
